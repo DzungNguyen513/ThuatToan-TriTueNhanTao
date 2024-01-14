a86ad2ba3fbbcf45c10b8b9e653c4ffe343348a9
doxuantrang2602 <xuantrang260203@gmail.com>
2024-01-15 03:20:03 +0700

    Merge Main_typeChar.java

--- src/BFS_char/Main_typeChar.java
+++ src/BFS_char/Main_typeChar.java
@@ -8,24 +8,26 @@ public class Main_typeChar {
 
 	public static void main(String[] args) {
 		Scanner sc  = new Scanner(System.in);
-		BFS_AI b = new BFS_AI();
-		b.input();
 		int lc = 0;
 		do {
 			System.out.println("------------------MENU-------------------");
 			System.out.println("|1. BFS theo đường đi từ 1 đỉnh cho trước|");
 			System.out.println("|2. BFS tìm đường đi đến 1 đỉnh bất kì   |");
 			System.out.println("|3. Thoát !                              |");
-			System.out.println("-----------------------------------------");
+			System.out.println("-----------------Group7-------------------");
 			System.out.print("Nhập lựa chọn: ");  lc = sc.nextInt();
 	        if(lc == 1) {
 	        	System.out.print("Nhập đỉnh bạn muốn đi: ");
-	        	int v = sc.nextInt();
+	        	char v = sc.next().charAt(0);
+	    		BFS_typeChar b = new BFS_typeChar();
+	    		b.input();
 	        	b.BFS_TimDuong(v);
 	        	System.out.println();
 	        } else if(lc == 2) {
 	        	System.out.print("Nhập đỉnh bạn muốn tìm: ");
-	        	int v = sc.nextInt();
+	        	char v = sc.next().charAt(0);
+	    		BFS_typeChar b = new BFS_typeChar();
+	    		b.input();
 	        	b.BFS_TimDinh(v);
 	        	System.out.println();
 	        } else if(lc == 3) {
@@ -33,7 +35,6 @@ public class Main_typeChar {
 	        }
 		}while (lc != 0);   
 		sc.close();
-
 	}
 
 }
