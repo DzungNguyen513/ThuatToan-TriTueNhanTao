223fd9540f115f7bde5855276668e29a975ad370
Do Xuan Trang <xuantrang260203@gmail.com>
2024-03-28 00:26:28 +0700

    Update done BFS_typeChar.java

--- src/BFS_char/BFS_typeChar.java
+++ src/BFS_char/BFS_typeChar.java
@@ -1,185 +1,181 @@
 package BFS_char;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class BFS_typeChar {
-    public int n, m;
-    public ArrayList<Character>[] adj; // Đánh dấu đường đi
-    public boolean[] visited; // Đánh dấu các đỉnh đã thăm
-    public String inPath = "D:\\Code_Java\\Java_Project\\LearningAI_Java\\src\\BFS_char\\input.txt";
-    public String outPath = "D:\\Code_Java\\Java_Project\\LearningAI_Java\\src\\BFS_char\\output.txt"; // đường dẫn file output
-    public FileWriter writer;
+   public int soCanh, soDinh;
+   public ArrayList<Character>[] adj; 
+   public boolean[] visited;
+   public String inPath = "D:\\Code_Java\\Java_Project\\LearningAI_Java\\src\\BFS_char\\input.txt";
+   public String outPath = "D:\\Code_Java\\Java_Project\\LearningAI_Java\\src\\BFS_char\\output.txt"; // đường dẫn file output
+   public FileWriter writer;
+   
+   public BFS_typeChar() {
+	   try {
+           writer = new FileWriter(outPath);
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+   }
+   
+   public void input() {
+	   try {
+		   FileInputStream fis = new FileInputStream(inPath);
+		   BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+		   String line = br.readLine(); // Đọc từng dòng
+		   String[] tokens = line.split(" ");
+		   soDinh = Integer.parseInt(tokens[0].toString());
+		   soCanh = Integer.parseInt(tokens[1].toString());
+		   
+		   adj = new ArrayList[1001];
+           visited = new boolean[1001];
+		   
+		   for(int i = 0;i<soDinh;i++) {
+			   adj[i] = new ArrayList<>(); // Khởi tạo đỉnh
+		   }
+		   
+		   for(int i = 0; i < soCanh;i++) {
+			   line = br.readLine();
+               tokens = line.split(" ");
+			   char x = tokens[0].charAt(0);
+			   char y = tokens[1].charAt(0);
+			   adj[x-'A'].add(y);
+		   }
+		   Arrays.fill(visited, false);
+		   br.close();
+	} catch (Exception e) {
+		
+	}
+   }
+   public void BFS_TimDuong(char dinh) {
+	   Queue<Character> q = new LinkedList<>();
+	   q.add(dinh);
+	   visited[dinh - 'A'] = true;
+	   System.out.print("=> ");
+	   while(!q.isEmpty()){
+		   char v = q.poll(); // Đỉnh đầu hàng đợi
+		   System.out.print(" "+v);
+		   if(adj[v - 'A'] != null) {
+			   for(Character x: adj[v-'A']) {
+				   if(!visited[x - 'A']) {
+					   q.add(x);
+					   visited[x-'A'] = true;
+				   }
+			   }
+		   }
+		   
+	   }
+	   Arrays.fill(visited, false);
+   }
+   
+   public void BFS_TimDinh(char dinh) {
+	   try {
+		   Queue<Character> q = new LinkedList<>();
+		   char[] path = new char[soDinh + 10]; // Đường đi
+		   boolean[] visited = new boolean[soDinh + 10]; // Mảng đánh dấu
+		   Arrays.fill(path, '\0');
+           Arrays.fill(visited, false);
+           q.add('A');
+           visited['A' - 'A'] = true;
+           path['A' - 'A'] = '\0';
+           
+           System.out.println("====================================================================");
+           writer.write("==========================================================================" + System.lineSeparator());
+           System.out.println("Phat trien trang thai\tTrang thai ke\t\tDanh Sach L");
+           writer.write("Phat trien trang thai\tTrang thai ke\t\tDanh Sach L" + System.lineSeparator());
+           System.out.println("====================================================================");
+           writer.write("==========================================================================" + System.lineSeparator());
+           boolean kt = false;
+           
+           while(!q.isEmpty()) {
+        	   char x = q.poll();
+        	   if (x == dinh) {
+                   kt = true;
+                   System.out.print("Found " + dinh);
+                   writer.write("Found " + dinh);
+                   System.out.println();
+                   writer.write(System.lineSeparator());
+                   System.out.print("=> ");
+                   writer.write("=> ");
+                   inDuongDi(path, dinh, true);
+                   System.out.println();
+                   writer.write(System.lineSeparator());
+                   break;
+               }
+        	   if (adj[x - 'A'] != null) {
+        		   // In Phát triển trạng thái
+        		   System.out.print(x + "\t\t\t");
+                   writer.write(x + "\t\t\t");
+                   System.out.print("|"); 
+                   writer.write("|");   
+                   
+                   // In Trạng thái kề
+                   for (char c : adj[x - 'A']) {
+                       System.out.print(c + " ");
+                       writer.write(c + " ");
+                   }
+                   
+                   // In trạng thái kề
+                   System.out.print("\t\t\t|"); 
+                   writer.write("\t\t\t|");
+                   for(char k: adj[x - 'A']) {
+                	   if(!visited[k - 'A']) {
+                		   q.add(k);
+                		   visited[k-'A'] = true;
+                		   path[k - 'A'] = x;
+                	   }
+                   }
+                   
+                   // In danh sách Queue
+                   inDSachL(q);
+        	   }
+           }
+	} catch (Exception e) {
+		
+	}
+   }
 
-    public BFS_typeChar() {
-        try {
-            writer = new FileWriter(outPath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+   public void inDSachL(Queue<Character> queue) {
+       try {
+           System.out.print("|");
+           writer.write("|");
 
-    public void input() {
-        try {
-            FileInputStream fis = new FileInputStream(inPath);
-            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-            n = Integer.parseInt(tokens[0]);
-            m = Integer.parseInt(tokens[1]);
-            adj = new ArrayList[1001];
-            visited = new boolean[1001];
+           for (char c : queue) {
+               System.out.print(c + " ");
+               writer.write(c + " ");
+           }
+           System.out.println();
+           writer.write(System.lineSeparator());
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+   }
 
-            for (int i = 0; i < n; i++) {
-                adj[i] = new ArrayList<>(); // Khởi mảng adj với n đỉnh
-            }
-            Arrays.fill(visited, false);
-
-            for (int i = 0; i < m; i++) {
-                line = br.readLine();
-                tokens = line.split(" ");
-                char x = tokens[0].charAt(0);
-                char y = tokens[1].charAt(0);
-                adj[x - 'A'].add(y);
-            }
-
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void BFS_TimDuong(char u) {
-        Queue<Character> q = new LinkedList<>();
-        q.add(u);
-        visited[u - 'A'] = true;
-        System.out.print("=> ");
-        while (!q.isEmpty()) {
-            char v = q.poll();
-            System.out.print(v + " ");
-
-            if (adj[v - 'A'] != null) {
-                for (Character x : adj[v - 'A']) {
-                    if (!visited[x - 'A']) {
-                        q.add(x);
-                        visited[x - 'A'] = true;
-                    }
-                }
-            }
-        }
-        Arrays.fill(visited, false);
-    }
-
-    public void BFS_TimDinh(char u) {
-        try {
-            Queue<Character> q = new LinkedList<>();
-            char[] path = new char[n + 10];
-            boolean[] visited = new boolean[n + 10];
-            Arrays.fill(path, '\0');
-            Arrays.fill(visited, false);
-
-            q.add('A');
-            visited['A' - 'A'] = true;
-            path['A' - 'A'] = '\0';
-
-            System.out.println("====================================================================");
-            writer.write("==========================================================================" + System.lineSeparator());
-            System.out.println("Phat trien trang thai\tTrang thai ke\t\tDanh Sach L");
-            writer.write("Phat trien trang thai\tTrang thai ke\t\tDanh Sach L" + System.lineSeparator());
-            System.out.println("====================================================================");
-            writer.write("==========================================================================" + System.lineSeparator());
-
-            boolean kt = false;
-
-            while (!q.isEmpty()) {
-                char x = q.poll();
-
-                if (x == u) {
-                    kt = true;
-                    System.out.print("Found " + u);
-                    writer.write("Found " + u);
-                    System.out.println();
-                    writer.write(System.lineSeparator());
-                    System.out.print("=> ");
-                    writer.write("=> ");
-                    inDuongDi(path, u, true);
-                    System.out.println();
-                    writer.write(System.lineSeparator());
-                    break;
-                }
-                if (adj[x - 'A'] != null) {
-                    System.out.print(x + "\t\t\t");
-                    writer.write(x + "\t\t\t");
-                    System.out.print("|");
-                    writer.write("|");
-                    for (char c : adj[x - 'A']) {
-                        System.out.print(c + " ");
-                        writer.write(c + " ");
-                    }
-                    System.out.print("\t\t\t|");
-                    writer.write("\t\t\t|");
-
-                    for (char k : adj[x - 'A']) {
-                        if (!visited[k - 'A']) {
-                            q.add(k);
-                            visited[k - 'A'] = true;
-                            path[k - 'A'] = x;
-                        }
-                    }
-                    inDSachL(q);
-                }
-            }
-            if (!kt) {
-                System.out.print("Not Found " + u + " !");
-                writer.write("Not Found " + u + " !");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-    public void inDuongDi(char[] p, char i, boolean isLast) {
-        try {
-            if (i == '\0') return;
-            inDuongDi(p, p[i - 'A'], false);
-            System.out.print(i);
-            writer.write(String.valueOf(i));
+   public void inDuongDi(char[] path, char dinh, boolean isLast) {
+    	try {
+    		if (dinh == '\0') return;
+    		inDuongDi(path, path[dinh - 'A'], false);
+    		System.out.print(dinh);
+            writer.write(String.valueOf(dinh));
             if (!isLast) {
                 System.out.print("->");
                 writer.write("->");
             }
-        } catch (IOException e) {
+		} catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public void inDSachL(Queue<Character> queue) {
-        try {
-            System.out.print("|");
-            writer.write("|");
-
-            for (char c : queue) {
-                System.out.print(c + " ");
-                writer.write(c + " ");
-            }
-            System.out.println();
-            writer.write(System.lineSeparator());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        }	
     }
 }
